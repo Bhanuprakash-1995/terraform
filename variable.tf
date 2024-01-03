96ce3987854b1074c589141ca7e2ef5006865d98
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-01-03 19:35:41 +0530

    3rd commit

--- variable.tf
+++ variable.tf
@@ -22,4 +22,22 @@ variable "subnet_id" {
   default = "subnet-01c58aba803ab2bc4"
 }
 
+variable "sg-name" {
+  type    = string
+  default = "roboshop-all-aws"
+}
 
+variable "sg-description" {
+  type    = string
+  default = "Allow TLS inbound traffic"
+}
+
+variable "inbound-from-port" {
+  #type = string
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type    = list(any)
+  default = ["0.0.0.0/0"]
+}
